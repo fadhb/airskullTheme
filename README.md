d78e0501eb12d8e57daa0b00e89856a491e1eded
Jonathan Headen <jonathan.headen@outlook.com>
2014-06-02 21:24:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+##Wordpress Child Theme 
+###based on Parallax by Themify
+Theme used on personal blog
---
+++ custom-functions.php
@@ -0,0 +1,6 @@
+<?php
+
+remove_filter( 'the_content', 'sharing_display', 19 );
+remove_filter( 'the_excerpt', 'sharing_display', 19 );
+ 
+?>
